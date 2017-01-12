1cfe37a2c31517afe3e447e328f298a03fc991cf
kurvin <kurvin@klarity.co.za>
2017-01-12 13:02:00 +0200

    Adding routing, working commit for now, need to sort out get movie by ID service

--- app/services/movie.service.ts
+++ app/services/movie.service.ts
@@ -10,4 +10,26 @@ export class MovieService {
         this.apikey = '787cc29e9b66bfa13c6904942d74beb4';
         console.log('Movieservice is initialized...');
     }
+
+    getPopular() {
+        //https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&sort_by=popularity.desc
+        return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&sort_by=popularity.desc&api_key='+this.apikey)
+            .map(res => res.json());
+    }
+
+    getInTheaters() {
+        //https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&sort_by=popularity.desc
+        return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&primary_release_date.gte=2017-01-01&primary_release_date.lte=2017-01-10&api_key='+this.apikey)
+            .map(res => res.json());
+    }
+
+    searchMovies(searchStr: string) {
+        return this._jsonp.get('https://api.themoviedb.org/3/search/movie?callback=JSONP_CALLBACK&query='+searchStr+'&sort_by=popularity.desc&api_key='+this.apikey)
+            .map(res => res.json());
+    }
+
+    getMovie(id: string) {
+        return this._jsonp.get('https://api.themoviedb.org/3/discover/movie/'+id+'?callback=JSONP_CALLBACK&api_key='+this.apikey)
+            .map(res => res.json());
+    }
 }
